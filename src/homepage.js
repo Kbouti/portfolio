6591f116e7147c2b36a93d65f6f327429680ae71
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-02-29 12:14:28 -0800

    Add text description to project cards

--- src/homepage.js
+++ src/homepage.js
@@ -7,10 +7,6 @@ const headShot1 = require("./images/IMG_2391.jpeg");
 const projectsFile = require("./projects");
 const projects = projectsFile.projects;
 
-
-
-
-
 function createElement(type, parentElement, id, classes) {
   const newElement = document.createElement(type);
   if (id !== "") {
@@ -45,52 +41,84 @@ function buildHeader() {
   firstPhoto.src = headShot1;
   headerPhotoContainer.appendChild(firstPhoto);
   const mainTitle = createElement("h1", headerPhotoContainer, "", [
-    "mainTitle", "title"
+    "mainTitle",
+    "title",
   ]);
   mainTitle.innerHTML = "Kevin Boutilier";
 
   const aboutSection = createElement("section", header, "", ["aboutSection"]);
-  const aboutTitle = createElement("h2", aboutSection, "", ["title", "sectionTitle"]);
+  const aboutTitle = createElement("h2", aboutSection, "", [
+    "title",
+    "sectionTitle",
+  ]);
   aboutTitle.innerHTML = "About me";
   const aboutText = createElement("p", aboutSection, "", ["aboutMeText"]);
   aboutText.innerHTML =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-  const widgetsContainer = createElement("div", header, "", ["widgetsContainer"]);
+  const widgetsContainer = createElement("div", header, "", [
+    "widgetsContainer",
+  ]);
 
-  const githubLogo = createElement("i", widgetsContainer, "", ["fa-github","fa","widget"]);
-  const linkedinLogo = createElement("i", widgetsContainer, "", ["fa-linkedin", "fa", "widget"]);
-
+  const githubLogo = createElement("i", widgetsContainer, "", [
+    "fa-github",
+    "fa",
+    "widget",
+  ]);
+  const linkedinLogo = createElement("i", widgetsContainer, "", [
+    "fa-linkedin",
+    "fa",
+    "widget",
+  ]);
 }
 
 function buildMain() {
   const main = createElement("main", body, "", ["mainContent"]);
   const contentTitle = createElement("h3", main, "", ["title", "sectionTitle"]);
   contentTitle.innerHTML = "My Work";
-  const projectsContainer = createElement("div", main, "", ["projectsContainer"]);
+  const projectsContainer = createElement("div", main, "", [
+    "projectsContainer",
+  ]);
 
+  // projects is an array containing project objects.
+  // We'll use this to build project cards for each one
 
-// projects is an array containing project objects. 
-// We'll use this to build project cards for each one
-
-for (let i = 0;i<projects.length;i++){
+  for (let i = 0; i < projects.length; i++) {
     console.log(projects[i].name);
 
-const newCard = createElement("div", projectsContainer, "", ["projectCard"]);
-const imageContainer = createElement("div", newCard, "", ["cardScreenshot"]);
-// Once we actually start storing the link to the image in the project object we can apply that here
+    const projectCard = createElement("div", projectsContainer, "", [
+      "projectCard",
+    ]);
+    const imageContainer = createElement("div", projectCard, "", [
+      "cardScreenshot",
+    ]);
+    // Once we actually start storing the link to the image in the project object we can apply that here
 
-const projectSubBar = createElement("div", newCard, "", ["projectSubBar"]);
-const projectName = createElement("p", projectSubBar, "", ["projectName"]);
-projectName.innerHTML = projects[i].name;
-const projectWidgetsContainer = createElement("div", projectSubBar, "", ["projectWidgetsContainer"]);
+    const projectSubBar = createElement("div", projectCard, "", ["projectSubBar"]);
+    const projectName = createElement("p", projectSubBar, "", ["projectName"]);
+    projectName.innerHTML = projects[i].name;
+    const projectWidgetsContainer = createElement("div", projectSubBar, "", [
+      "projectWidgetsContainer",
+    ]);
 
-const gitHubIcon = createElement("i", projectWidgetsContainer, "", ["fa-github","fa","widget"])
+    const gitHubIcon = createElement("i", projectWidgetsContainer, "", [
+      "fa-github",
+      "fa",
+      "widget",
+    ]);
 
-const arrowUpRightIcon = createElement("span", projectWidgetsContainer, "", ["material-symbols-outlined"])
-arrowUpRightIcon.innerHTML = "open_in_new"
+    const arrowUpRightIcon = createElement(
+      "span",
+      projectWidgetsContainer,
+      "",
+      ["material-symbols-outlined"]
+    );
+    arrowUpRightIcon.innerHTML = "open_in_new";
 
-}
 
+const projectDescription = createElement("p", projectCard, "", ["projectCDescription"]);
+projectDescription.innerHTML = projects[i].description;
+
+  }
 }
 
 function buildFooter() {
