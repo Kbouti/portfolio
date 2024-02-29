428aaa2eff962920ec58f901243a20ddff9ca093
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-02-28 16:50:05 -0800

    Center about section header

--- src/homepage.js
+++ src/homepage.js
@@ -42,13 +42,15 @@ function buildHeader() {
   ]);
   mainTitle.innerHTML = "Kevin Boutilier";
 
-  const aboutSection = createElement("section", header, "");
+  const aboutSection = createElement("section", header, "", ["aboutSection"]);
   const aboutTitle = createElement("h2", aboutSection, "");
   aboutTitle.innerHTML = "About me";
-
   const aboutText = createElement("p", aboutSection, "", ["aboutMeText"]);
   aboutText.innerHTML =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  const widgetsContainer = createElement("div", header, "", ["widgetsContainer"]);
+  // ^^ Put github, discord, linkedIn logos here
+
 }
 
 function buildMain() {
