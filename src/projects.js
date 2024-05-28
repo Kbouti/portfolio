a88442e4f1873559448d1cc6db24834779fdd7cb
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-28 13:26:54 -0700

    Add shopping cart project

--- src/projects.js
+++ src/projects.js
@@ -11,17 +11,27 @@ class Project {
   }
 }
 
+const shoppingCartProject = new Project(
+  "Shopping Cart",
+  "Basic simulation of an online store using React. Products imported from an API. "
+);
+
+shoppingCartProject.Image = require("./images/projectScreenshots/shoppingCart.png");
+shoppingCartProject.Image.alt = "Online web store";
+shoppingCartProject.projectLink = "https://shopping-cart-beryl-two.vercel.app/";
+shoppingCartProject.repoLink = "https://github.com/Kbouti/shoppingCart";
+projects.push(shoppingCartProject);
+
 const memoryCardProject = new Project(
   "Memory Card",
   "Test your memory with dog photos. Try not to click the same card twice to beat your high score. Built using React"
-)
+);
 
 memoryCardProject.Image = require("./images/projectScreenshots/memoryCard.png");
 memoryCardProject.Image.alt = "A game featuring several photos of dogs";
 memoryCardProject.projectLink = "https://memory-card-eight-sand.vercel.app/";
 memoryCardProject.repoLink = "https://github.com/Kbouti/memoryCard";
 projects.push(memoryCardProject);
-
 
 const cvProject = new Project(
   "CV-Project",
@@ -34,14 +44,14 @@ cvProject.projectLink = "https://cv-project-pi-lovat.vercel.app/";
 cvProject.repoLink = "https://github.com/Kbouti/CV-Project";
 projects.push(cvProject);
 
-
 const battleShipProject = new Project(
   "Battleship",
   "The classic board game brought to the web. Sink all of your opponent's ships before they sink yours! "
 );
 // battleShipProject.Image = require("./images/screenshots/battle.png");
 battleShipProject.Image = require("./images/projectScreenshots/battleship.png");
-battleShipProject.Image.alt = "User interface resembling the classic battleship boardgame"
+battleShipProject.Image.alt =
+  "User interface resembling the classic battleship boardgame";
 battleShipProject.projectLink = "https://kbouti.github.io/Battleship/";
 battleShipProject.repoLink = "https://github.com/Kbouti/Battleship";
 projects.push(battleShipProject);
@@ -52,7 +62,7 @@ const toDoListProject = new Project(
 );
 // toDoListProject.Image = require("./images/screenshots/toDo.png");
 toDoListProject.Image = require("./images/projectScreenshots/toDoList.png");
-toDoListProject.alt = "A to-do list applicaton showing various tasks"
+toDoListProject.alt = "A to-do list applicaton showing various tasks";
 toDoListProject.projectLink = "https://kbouti.github.io/toDo/";
 toDoListProject.repoLink = "https://github.com/Kbouti/toDo";
 projects.push(toDoListProject);
@@ -63,7 +73,7 @@ const ticTacToeProject = new Project(
 );
 // ticTacToeProject.Image = require("./images/screenshots/tic.png");
 ticTacToeProject.Image = require("./images/projectScreenshots/ticTacToe.png");
-ticTacToeProject.alt = "A game of tic-tac-toe"
+ticTacToeProject.alt = "A game of tic-tac-toe";
 ticTacToeProject.projectLink = "https://kbouti.github.io/ticGame/";
 ticTacToeProject.repoLink = "https://github.com/Kbouti/ticGame";
 projects.push(ticTacToeProject);
@@ -74,7 +84,7 @@ const adminDashboardProject = new Project(
 );
 
 adminDashboardProject.Image = require("./images/projectScreenshots/adminDashboard.png");
-adminDashboardProject.alt = "A professional looking social media site"
+adminDashboardProject.alt = "A professional looking social media site";
 adminDashboardProject.projectLink = "https://kbouti.github.io/adminDashboard/";
 adminDashboardProject.repoLink = "https://github.com/Kbouti/adminDashboard";
 projects.push(adminDashboardProject);
@@ -85,7 +95,7 @@ const calculatorProject = new Project(
 );
 // calculatorProject.Image = require("./images/screenshots/calc.png");
 calculatorProject.Image = require("./images/projectScreenshots/calculator.png");
-calculatorProject.alt = "A calculator"
+calculatorProject.alt = "A calculator";
 calculatorProject.projectLink = "https://kbouti.github.io/calculator/";
 calculatorProject.repoLink = "https://github.com/Kbouti/calculator";
 projects.push(calculatorProject);
@@ -96,7 +106,7 @@ const etchASketchProject = new Project(
 );
 // etchASketchProject.Image = require("./images/screenshots/etch.png");
 etchASketchProject.Image = require("./images/projectScreenshots/etchASketch.png");
-etchASketchProject.alt = "A virtual Etch-A-Sketch"
+etchASketchProject.alt = "A virtual Etch-A-Sketch";
 etchASketchProject.projectLink = "https://kbouti.github.io/etchy-sketchy/";
 etchASketchProject.repoLink = "https://github.com/Kbouti/etchy-sketchy";
 
@@ -108,7 +118,7 @@ const rockPaperScissorsProject = new Project(
 );
 // rockPaperScissorsProject.Image = require("./images/screenshots/rock.png");
 rockPaperScissorsProject.Image = require("./images/projectScreenshots/rockPaperScissors.png");
-rockPaperScissorsProject.alt = "User interface for a rock paper scissors game"
+rockPaperScissorsProject.alt = "User interface for a rock paper scissors game";
 rockPaperScissorsProject.projectLink =
   "https://kbouti.github.io/rock-paper-scissors/";
 rockPaperScissorsProject.repoLink =
